cd10232021be77bb04f511afe8e00d1905a5a2a1
kidbofill02 <mbofillg01@educantabria.es>
2026-03-25 15:50:33 +0100

    Repositorio nuevo

---
+++ Examen/src/main/java/examen/examen/Examen.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package examen.examen;
+
+/**
+ *!
+ * @author Alumno
+ */
+public class Examen {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
