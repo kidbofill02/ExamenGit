a0a7132841627012195862d61ece433dcee43c78
kidbofill02 <mbofillg01@educantabria.es>
2026-03-25 15:54:17 +0100

    Realizado el primer commit

--- Examen/src/main/java/examen/examen/Examen.java
+++ Examen/src/main/java/examen/examen/Examen.java
@@ -11,6 +11,6 @@ package examen.examen;
 public class Examen {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Alex Bofill!");
     }
 }
